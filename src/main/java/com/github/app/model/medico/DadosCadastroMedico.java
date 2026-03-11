c11d38566e820cbaf126ea19364b7dadaf202bbf
Lara Peddinghausen <larapedd@gmail.com>
2026-03-11 19:33:33 -0300

    first commit

---
+++ src/main/java/com/github/app/model/medico/DadosCadastroMedico.java
@@ -0,0 +1,18 @@
+package com.github.app.model.medico;
+
+import com.github.app.model.endereco.DadosCadastroEndereco;
+
+//Essa classe é responsável por pegar os dados que estão vindo do simulador de requisição (insomnia) e transformar em atributos (variáveis) de uma entidade chamada Médico.
+
+//A classe do tipo record já cria todos os getters, setters, constructors, hashcode e equals.
+
+
+public record DadosCadastroMedico(
+    String nome, 
+    String email, 
+    String crm, 
+    Especialidade especialidade,  //uma classe só para especialidades, onde deixo pré definido quais são as especialidades aceitas pelo sistema
+    DadosCadastroEndereco endereco  //uma classe para endereço, pois são informações que irão se repetir também para pacientes
+    ) {
+    
+}
---
+++ src/main/java/com/github/app/model/medico/Medico.java
@@ -0,0 +1,39 @@
+package com.github.app.model.medico;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//Classe modelo responsável por criar uma tabela e suas colunas no BD.
+
+@Getter //Lombok - cria get para todos os atributos
+@Setter //Lombok - cria set para todos os atributos
+@AllArgsConstructor // Lombok - cria um construtor com todos os atributos
+@NoArgsConstructor  //Lombok - cria um construtor sem atributos
+@EqualsAndHashCode(of = "id")  //Lombok - cria uma lógica de comparação através do campo 'id'
+@Entity //SPRING JPA - informa que a classe abaixo é uma entidade, ou seja, será uma tabela no BD
+@Table(name = "medicos") //SPRING JPA - (opcional) - gera uma tabela com nome médicos no BD
+public class Medico {
+
+    @Id //SPRING JPA - informa para o BD que a chave primária é o id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //SPRING JPA - cria o id único de forma automática
+    private Integer id; // id não está vindo do insomnia. Integer = classe wrapper = classes que "embrulham" (wrap) tipos primitivos (int, char, double, etc.) em objetos, permitindo utilizá-los onde objetos são necessários. Elas fornecem métodos úteis para conversão de tipos (ex: Integer.parseInt()) e permitem que valores sejam null. 
+
+    private String nome;
+    private String email;
+    private String crm;
+    
+    @Enumerated(EnumType.STRING) //SPRING JPA - informa para o BD que o atributo é do tipo enum
+    private Especialidade especialidade;
+
+
+}
