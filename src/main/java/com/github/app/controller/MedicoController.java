372c2b19ea2ef7819cf42b1292047ad72939ad48
Daniel Moura Carreiro <mouradaniel0818@gmail.com>
2026-03-30 21:15:05 -0300

    modificacoes na controller e DadosListagemMedico

--- src/main/java/com/github/app/controller/MedicoController.java
+++ src/main/java/com/github/app/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package com.github.app.controller;
 
+import com.github.app.AppApplication;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;  //já importa todos os que estão comentados abaixo
 
 // import org.springframework.web.bind.annotation.PostMapping;
@@ -9,18 +14,49 @@ import org.springframework.web.bind.annotation.*;  //já importa todos os que es
 // import org.springframework.web.bind.annotation.RestController;
 
 import com.github.app.model.medico.DadosCadastroMedico;
+import com.github.app.model.medico.DadosListagemMedico;
 import com.github.app.model.medico.Medico;
 import com.github.app.model.medico.MedicoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController //SPRING WEB - Informa para o Springboot que abaixo é uma classe controladora de requisições (GET-POST-PUT-DELETE)
 @RequestMapping("/medicos") //SPRING WEB - Cria um caminho (endpoint) para a classe MedicoCotroller
 public class MedicoController {
 
+    private final AppApplication appApplication;
     @Autowired //Sobreescrevendo algo. É um padrão utilizado na injeção de dependência.
     private MedicoRepository repository; //tem que importar
+
+    MedicoController(AppApplication appApplication) {
+        this.appApplication = appApplication;
+    }
     
     @PostMapping //SPRING WEB - Informa que o método abaixo é do tipo POST (cadastrar)
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
+
+    @GetMapping("todos") // informa que o método abaixo é do tipo GET (buscar/ler)
+    public List<Medico> listarTodos() {
+        return repository.findAll();
+    }
+    @GetMapping // informa que o método abaixo é do tipo GET (buscar/ler)
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+        //findAll() -> Método que retorna uma lista de objeto do tipo DadosListagemMedico. 
+        //stream() -> Método utilizado para transformar a lista em um fluxo de dados, permitindo aplicar operações de transformações.
+        //map(DadosListagemMedico::new) -> Método utilizado para converter cada objeto do tipo medico em um json DadosListagemMedico, utilizando o constructor que criamos em DadosListagemMedico. 
+        //toList() -> Método utilizado para coletar os resulatados em uma nova lista do tipo DadosListagemMedico, que é o formato que queremos retornar para API.
+     }
+
+     @GetMapping
+    public Page<DadosListagemMedico> listarPorPagina(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+
+    }
+
+
+    
 }
---
+++ src/main/java/com/github/app/model/medico/DadosListagemMedico.java
@@ -0,0 +1,16 @@
+package com.github.app.model.medico;
+
+public record DadosListagemMedico(
+    String nome,
+    String email,
+    String crm,
+    Especialidade especialidade
+) {
+    // Método construtor recebendo o objeto Médico e convertendo para Json DadosListagemMedico.
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+    // o this é para chamar o constructor do record, passando do médico para preencher os atributos da classe.
+    // O constructor acima é utilizado para converter um objeto tipo Medico em um json do tipo DadosListagemMedico, que é o formato que queremos devolver pra nossa API.
+    
+}
